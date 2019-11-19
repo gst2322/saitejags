6def10c3d71a87e0d15218e124c5fc3bb385e13b
gst2322 <saiteja2322@gmail.com>
2019-11-20 01:33:34 +0530

    Updates

---
+++ precache-manifest.c53fdcffd4e3492f0657d5a86a2e032b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4538050e7dceb1b4b644",
+    "url": "/saitejags/static/css/main.e4d50baa.chunk.css"
+  },
+  {
+    "revision": "4538050e7dceb1b4b644",
+    "url": "/saitejags/static/js/main.4538050e.chunk.js"
+  },
+  {
+    "revision": "681cafecbe3656b70cec",
+    "url": "/saitejags/static/css/1.f99e53de.chunk.css"
+  },
+  {
+    "revision": "681cafecbe3656b70cec",
+    "url": "/saitejags/static/js/1.681cafec.chunk.js"
+  },
+  {
+    "revision": "4cc6c1cd66b04d4e71f2",
+    "url": "/saitejags/static/js/runtime~main.4cc6c1cd.js"
+  },
+  {
+    "revision": "1eaf61bc7221d7a45a60eb346f3a6a46",
+    "url": "/saitejags/static/media/saiteja.1eaf61bc.png"
+  },
+  {
+    "revision": "02b04ab9206495ce2835a02a8713c1f3",
+    "url": "/saitejags/static/media/fullpic.02b04ab9.jpg"
+  },
+  {
+    "revision": "34724e38250ab89f08e931037a9afced",
+    "url": "/saitejags/static/media/carpr.34724e38.png"
+  },
+  {
+    "revision": "0195d07fab961430d2ebf684c47fa1c2",
+    "url": "/saitejags/static/media/indpr.0195d07f.png"
+  },
+  {
+    "revision": "e1d334a76d866ba8a3601359959cc497",
+    "url": "/saitejags/static/media/colorpr.e1d334a7.png"
+  },
+  {
+    "revision": "8161fe8c15fa57486ea330ba2f18efa5",
+    "url": "/saitejags/static/media/resume.8161fe8c.jpg"
+  },
+  {
+    "revision": "26c00dc0d61037f62dcda20beb668c97",
+    "url": "/saitejags/static/media/resumepdf.26c00dc0.pdf"
+  },
+  {
+    "revision": "de79e794762a675fccdbaddcd73e6d03",
+    "url": "/saitejags/index.html"
+  }
+];
